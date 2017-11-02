13b9ad4c4f0a29cb37bc483b7e81b054553f7228
24830 <yuukl248@icloud.com>
2017-11-02 11:08:31 +0900

    init

---
+++ sample1.cpp
@@ -0,0 +1,42 @@
+#include <iostream> 
+#include <cstdio>
+#include <cstdlib> 
+using namespace std; 
+
+int main() 
+{ 
+  FILE *fp = popen("gnuplot", "w"); 
+  if (fp == NULL)  return -1; 
+  fputs("set mouse\n", fp); 
+  fputs("set size ratio -1\n",fp);
+  fputs("set xrange[-1:1]\n",fp);
+  fputs("set yrange[-1:1]\n", fp);
+  fputs("unset key\nunset border\nunset xtics\nunset ytics\n",fp);
+  fputs("set angles degrees\n",fp);
+  fputs("set style fill transparent solid 0.4 border lc rgb \"black\"\n",fp);
+  fputs("set style lines 1 lc rgb \"yellow\"\n",fp);
+  fputs("set style lines 2 lc rgb \"cyan\"\n",fp);
+  fputs("set style lines 3 lc rgb \"brown\"\n",fp);
+  fputs("set style lines 4 lc rgb \"magenta\"\n",fp);
+  fputs("set style lines 5 lc rgb \"gray\"\n",fp);
+  fputs("angle_conv(x) = -x +90.0\n",fp);
+  fputs("plot \"circle-data0.dat\"using (0):(0):(1):(angle_conv($3)):(angle_conv($2)):($0+1) with circles lc var,\"\"using(0.7*cos(((angle_conv($2)+angle_conv($3))/2.0))):(0.7*sin(((angle_conv($2)+angle_conv($3))/2.0))):1 with labels\n",fp);
+  fflush(fp); 
+
+
+	cout<<"a"<<endl ; 
+  	cin.get();
+	cout<<"a"<<endl ; 
+	return 0;
+  	cout<<"a"<<endl ; 
+  	pclose(fp); 
+  	cout<<"a"<<endl ;  
+
+	char string;
+	cin>>string;
+	if(string == "end"){
+		exit(1);
+	}
+
+
+} 
