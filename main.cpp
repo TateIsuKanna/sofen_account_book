e327e1c504ea2d589be45d96d86169365ad0c061
TateIsuKanna <TateIsuKanna@users.noreply.github.com>
2017-11-27 13:06:10 +0900

    実装

--- main.cpp
+++ main.cpp
@@ -76,34 +76,39 @@ void signal_handler(int signal_n){
 }
 
 void add_command(){
-	string date_str;
-	string name;
-	money value;
-	cin>>date_str>>name>>value;
+	while(true){
+		string date_str;
+		string name;
+		money value;
+		cin>>date_str;
+		if(date_str=="q"){
+			return;
+		}
+		cin>>name>>value;
+		//日付補完．今日の日付を元に，入力した部分だけ置き換えする．
+		//例えば，2011/11/12 で入力が 11 なら 2011/11/11 にする．
+		time_t date=time(nullptr);
+		tm* lt=localtime(&date);
 
-	//日付補完．今日の日付を元に，入力した部分だけ置き換えする．
-	//例えば，2011/11/12 で入力が 11 なら 2011/11/11 にする．
-	time_t date=time(nullptr);
-	tm* lt=localtime(&date);
+		regex date_re(R"((((\d{4,})/)?([1-9]|1[0-2])/)?([1-3]\d|[1-9]))");
+		sregex_token_iterator it;
+		it=sregex_token_iterator(begin(date_str), end(date_str), date_re, 3);
+		if(it->str()!=""){//HACK:多分
+			lt->tm_year=stoi(it->str())-1900;//HACK:マジックナンバーでは
+		}
+		it=sregex_token_iterator(begin(date_str), end(date_str), date_re, 4);
+		if(it->str()!=""){//HACK:多分
+			lt->tm_mon=stoi(it->str())-1;//HACK:マジックナンバーでは
+		}
+		it=sregex_token_iterator(begin(date_str), end(date_str), date_re, 5);
+		if(it->str()!=""){//HACK:多分
+			lt->tm_mday=stoi(it->str());
+		}
 
-	regex date_re(R"((((\d{4,})/)?([1-9]|1[0-2])/)?([1-3]\d|[1-9]))");
-	sregex_token_iterator it;
-	it=sregex_token_iterator(begin(date_str), end(date_str), date_re, 3);
-	if(it->str()!=""){//HACK:多分
-		lt->tm_year=stoi(it->str())-1900;//HACK:マジックナンバーでは
+		date=mktime(lt);
+
+		master.add_record(date,name,value);
 	}
-	it=sregex_token_iterator(begin(date_str), end(date_str), date_re, 4);
-	if(it->str()!=""){//HACK:多分
-		lt->tm_mon=stoi(it->str())-1;//HACK:マジックナンバーでは
-	}
-	it=sregex_token_iterator(begin(date_str), end(date_str), date_re, 5);
-	if(it->str()!=""){//HACK:多分
-		lt->tm_mday=stoi(it->str());
-	}
-
-	date=mktime(lt);
-
-	master.add_record(date,name,value);
 }
 
 void calc_rate(){
